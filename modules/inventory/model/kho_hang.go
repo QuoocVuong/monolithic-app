3ed6453a94ef4c94ca5bf720590dfe51429c9a47
Quoocvuong <lanhvuong2607@gmail.com>
2024-10-14 13:05:22 +0700

    them corsMiddleware vao main

--- modules/inventory/model/kho_hang.go
+++ modules/inventory/model/kho_hang.go
@@ -16,10 +16,12 @@ func (KhoHang) TableName() string { return "kho_hangs" }
 // Các struct cho Create, Update, Filter, ... (nếu cần)
 // Ví dụ:
 type KhoHangCreate struct {
+	common.SQLModel
 	TenKho string `json:"tenKho" binding:"required"` // required: bắt buộc phải có khi tạo
 }
 
 type KhoHangUpdate struct {
+	common.SQLModel
 	TenKho *string `json:"tenKho" ` //  ko bắt buộc phải có khi update
 }
 
